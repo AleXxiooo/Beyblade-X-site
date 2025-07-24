e1a90c4999c87247a0e6fbaa76056c264f3acb68
AleXxiooo <oloekolek@wp.pl>
2025-07-24 10:58:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -143,4 +143,8 @@ document.addEventListener("DOMContentLoaded", () => {
   populateSelect("selectBlade", bladeData, "Blade");
   populateSelect("selectRatchet", ratchetData, "Ratchet");
   populateSelect("selectBit", bitData, "Bit");
+
+  populateSelect("selectBlade2", bladeData, "Blade");
+  populateSelect("selectRatchet2", ratchetData, "Ratchet");
+  populateSelect("selectBit2", bitData, "Bit");
 });
